d0b9525678a3d87e5104ddc645fb125556ab3b2e
Abhilash <abhilashbabuj@gmail.com>
2019-01-09 19:49:27 +0100

    Moved python backend to python folder

--- backend/python/file_capture.py
+++ backend/python/file_capture.py
@@ -2,13 +2,19 @@ import cv2
 import glob
 import os
 import os.path as osp
+import time
 
 def read_image(params):
     file_path = params['file_path']
     image_files = glob.glob(osp.join(file_path, "*.jpg"))
+    print(image_files)
     for f in image_files:
+        print(f)
         img = cv2.imread(f)
+        print(img.shape)
+        time.sleep(20)
         yield img
+    raise StopIteration
 
 def do_file_capture(*args, **kwargs):
     params = {}
@@ -18,3 +24,10 @@ def do_file_capture(*args, **kwargs):
         outputs = kwargs['outputs']
         img_generator = read_image(params)
         outputs['image'] = next(img_generator)
+
+if __name__ == "__main__":
+    try:
+        while True:
+            do_file_capture(params={'file_path' : 'D:\\images'}, outputs = {})
+    except:
+        println("Done")
